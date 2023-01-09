aca087dbf9160a2aabd619a2fe4f200e9d6eaaee
Joshua Romani <119004668+DongleJosh@users.noreply.github.com>
2023-01-09 16:31:56 -0600

    html css js
    initial code

---
+++ script.js
@@ -0,0 +1,105 @@
+
+window.onload = function () {
+
+    var questionArea = document.getElementsByClassName('questions')[0],
+        answerArea   = document.getElementsByClassName('answers')[0],
+        checker      = document.getElementsByClassName('checker')[0],
+        current      = 0,
+    
+       // An object that holds all the questions + possible answers.
+       // In the array --> last digit gives the right answer position
+        allQuestions = {
+          'What is NOT a style of pizza?'  : ['Altoona', 'New York', 'Neopolitan', 'Greek', 'Gatekeeping Pizza is Wrong', 4],
+          
+          'Will I pass this course' : ['Yes', 'Maybe' , 'No', 1],
+          
+          'Why am I still trying to succeed in life' : ['Trying not to be homeless', 'Fear of dying alone', 'My parents are telling me to', 1]
+
+          'Is there a God' : ['Maybe']
+        };
+        
+    function loadQuestion(curr) {
+    // This function loads all the question into the questionArea
+    // It grabs the current question based on the 'current'-variable
+    
+      var question = Object.keys(allQuestions)[curr];
+      
+      questionArea.innerHTML = '';
+      questionArea.innerHTML = question;    
+    }
+    
+    function loadAnswers(curr) {
+    // This function loads all the possible answers of the given question
+    // It grabs the needed answer-array with the help of the current-variable
+    // Every answer is added with an 'onclick'-function
+    
+      var answers = allQuestions[Object.keys(allQuestions)[curr]];
+      
+      answerArea.innerHTML = '';
+      
+      for (var i = 0; i < answers.length -1; i += 1) {
+        var createDiv = document.createElement('div'),
+            text = document.createTextNode(answers[i]);
+        
+        createDiv.appendChild(text);      
+        createDiv.addEventListener("click", checkAnswer(i, answers));
+        
+        
+        answerArea.appendChild(createDiv);
+      }
+    }
+    
+    function checkAnswer(i, arr) {
+      // This is the function that will run, when clicked on one of the answers
+      // Check if givenAnswer is sams as the correct one
+      // After this, check if it's the last question:
+      // If it is: empty the answerArea and let them know it's done.
+      
+      return function () {
+        var givenAnswer = i,
+            correctAnswer = arr[arr.length-1];
+        
+        if (givenAnswer === correctAnswer) {
+          addChecker(true);             
+        } else {
+          addChecker(false);                        
+        }
+        
+        if (current < Object.keys(allQuestions).length -1) {
+          current += 1;
+          
+          loadQuestion(current);
+          loadAnswers(current);
+        } else {
+          questionArea.innerHTML = 'EZ PZ Lemon Squeezy';
+          answerArea.innerHTML = '';
+        }
+                                
+      };
+    }
+    
+    function addChecker(bool) {
+    // This function adds a div element to the page
+    // Used to see if it was correct or false
+    
+      var createDiv = document.createElement('div'),
+          txt       = document.createTextNode(current + 1);
+      
+      createDiv.appendChild(txt);
+      
+      if (bool) {
+        
+        createDiv.className += 'correct';
+        checker.appendChild(createDiv);
+      } else {
+        createDiv.className += 'false';
+        checker.appendChild(createDiv);
+      }
+    }
+    
+    
+    // Start the quiz right away
+    loadQuestion(current);
+    loadAnswers(current);
+    
+  };
